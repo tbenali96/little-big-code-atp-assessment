93d98b263c23ff070193a08a28b82cecc0f8d1e1
tbenali96 <benali.tahayassine@gmail.com>
2022-07-29 12:26:11 +0200

    Réorganisation du code

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,5 +1,9 @@
 import math
 import pandas as pd
+from pickle import dump
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
 
 
 def extract_month_from_date(df: pd.DataFrame, column_to_use: str, column_to_create: str) -> pd.DataFrame:
@@ -23,3 +27,37 @@ def define_ranking_category(df: pd.DataFrame, column_to_use: str, column_to_crea
 
     df[column_to_create] = df[column_to_use].apply(lambda x: define_rank(x))
     return df
+
+
+def remove_irrelevant_columns(df: pd.DataFrame, columns: list) -> pd.DataFrame:
+    return df.drop(columns=columns)
+
+
+def fill_na_for_numeric_columns_with_mean(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    df[column] = df[column].fillna(df[column].mean())
+    return df
+
+
+def fill_na_for_categorical_columns(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    col_imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+    col_imputer.fit(df[[column]])
+    df[column] = col_imputer.transform(df[[column]])
+    return df
+
+
+def fill_na_for_categorical_columns_with_condition(df: pd.DataFrame, column: str, condition: bool) -> pd.DataFrame:
+    col_imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+    col_imputer.fit(df[condition][[column]])
+    df.loc[condition, column] = col_imputer.transform(df[condition][[column]])
+    return df
+
+
+def scale_features(df: pd.DataFrame, columns_to_scale: list) -> pd.DataFrame:
+    scaler = StandardScaler()
+    scaler.fit(df[columns_to_scale])
+    df[columns_to_scale] = scaler.transform(df[columns_to_scale])
+    dump(scaler, open('../models/scaler.pkl', 'wb'))
+    return df
+
+
+
